b0faa58816ab687fd2897f97bf44ec4d5841517e
IvanBirtic <73253407+IvanBirtic@users.noreply.github.com>
2022-02-18 14:06:07 +0100

    Delete zadaci directory

--- zadaci/zad12/cookie.php
+++
@@ -1,6 +0,0 @@
-<?php
-	
-	setcookie('user', $_POST['user'], time() + (86400 * 30), "/"); // 86400 = 1 day
-	header("Location: index.php");
-	   
-?> 
--- zadaci/zad12/odjava.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if (isset($_COOKIE['user'])) {
-		
-		setcookie('user', null, -1, '/');
-	}
-	header("Location: index.php");
-?>
